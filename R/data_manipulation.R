07866609c3363e60e6f8c8e1d82a43651770879c
Ben Sabath <mbsabath@hsph.harvard.edu>
2019-10-24 12:32:45 -0400

    add 9 digit zip support

--- R/data_manipulation.R
+++ R/data_manipulation.R
@@ -14,8 +14,19 @@ reverse_string <- function(str) {
 #'
 #' @param zip zipcode represented as an integer
 #'
+#' @details This function initially assumes that the input is intended to be  a 5 digit zipcode.
+#'     If the initial conversion is more than 5 characters, the input is assumed to be intended to
+#'     be a 9 digit zipcode. The function still returns the 5 digit equivalent of the 9 digit
+#'     zip code.
+#'
 #' @export
 #'
 int_to_zip_str <- function(zip) {
-  return(sprintf("%.5d",zip))
+
+  out <- sprintf("%.5d",zip)
+  if (nchar(out) > 5) {
+    out <- sprintf("%.9d",zip)
+    out <- substr(out,1,5)
+  }
+  return(out)
 }
