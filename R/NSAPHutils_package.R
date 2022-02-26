43953d364edda2979e84137a2bc26faae5c33d62
naeemkh <khoshnevis.naeem@gmail.com>
2022-02-26 12:03:02 -0500

    polish description.

---
+++ R/NSAPHutils_package.R
@@ -0,0 +1,19 @@
+#' @title
+#' The 'NSAPHutils' package.
+#'
+#' @description
+#' An R package to provide processing tools to facilitate research at NSAPH.
+#'
+#' @details
+#' TBD
+#'
+#' @docType package
+#' @name NSAPHutils-package
+#' @aliases NSAPHutils
+#' @author TBD
+#' @author
+#'
+#'
+#' @references
+#' TBD
+NULL
